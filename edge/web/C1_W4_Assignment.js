b950fa7f16efdf60883de8947ce35ffc40049e40
sapategu <gultomterampil19@gmail.com>
2024-11-25 01:27:30 +0700

    update code implementations

--- edge/web/C1_W4_Assignment.js
+++ edge/web/C1_W4_Assignment.js
@@ -25,20 +25,20 @@ async function train() {
   // using ReLu activation functions where applicable.
   model = tf.sequential({
     layers: [
-        
-      // YOUR CODE HERE
-
+      tf.layers.flatten({inputShape: mobilenet.outputs[0].shape.slice(1)}),
+      tf.layers.dense({ units: 100, activation: "relu"}),
+      tf.layers.dense({ units: 5, activation: "softmax"})
     ]
   });
     
    
   // Set the optimizer to be tf.train.adam() with a learning rate of 0.0001.
-  const optimizer = // YOUR CODE HERE
+  const optimizer = tf.train.adam(0.0001)
     
         
   // Compile the model using the categoricalCrossentropy loss, and
   // the optimizer you defined above.
-  model.compile(// YOUR CODE HERE);
+  model.compile({optimizer: optimizer, loss: "categoricalCrossentropy", metrics: ["accuracy"]});
  
   let loss = 0;
   model.fit(dataset.xs, dataset.ys, {
@@ -54,34 +54,31 @@ async function train() {
 
 
 function handleButton(elem){
-	switch(elem.id){
-		case "0":
-			rockSamples++;
-			document.getElementById("rocksamples").innerText = "Rock samples:" + rockSamples;
-			break;
-		case "1":
-			paperSamples++;
-			document.getElementById("papersamples").innerText = "Paper samples:" + paperSamples;
-			break;
-		case "2":
-			scissorsSamples++;
-			document.getElementById("scissorssamples").innerText = "Scissors samples:" + scissorsSamples;
-			break;  
-		case "3":
-			spockSamples++;
-			document.getElementById("spocksamples").innerText = "Spock samples:" + spockSamples;
-			break;
-            
-        // Add a case for lizard samples.
-        // HINT: Look at the previous cases.
-            
-        // YOUR CODE HERE
-		
-            
-	}
-	label = parseInt(elem.id);
-	const img = webcam.capture();
-	dataset.addExample(mobilenet.predict(img), label);
+  switch(elem.id){
+    case "0":
+      rockSamples++;
+      document.getElementById("rocksamples").innerText = "Rock samples:" + rockSamples;
+      break;
+    case "1":
+      paperSamples++;
+      document.getElementById("papersamples").innerText = "Paper samples:" + paperSamples;
+      break;
+    case "2":
+      scissorsSamples++;
+      document.getElementById("scissorssamples").innerText = "Scissors samples:" + scissorsSamples;
+      break;
+    case "3":
+      spockSamples++;
+      document.getElementById("spocksamples").innerText = "Spock samples:" + spockSamples;
+      break;
+    case "4":
+      lizardSamples++;
+      document.getElementById("lizardsamples").innerText = "Lizard samples:" + lizardSamples;
+      break;
+  }
+  label = parseInt(elem.id);
+  const img = webcam.capture();
+  dataset.addExample(mobilenet.predict(img), label);
 
 }
 
@@ -89,40 +86,42 @@ async function predict() {
   while (isPredicting) {
     const predictedClass = tf.tidy(() => {
       const img = webcam.capture();
-      const activation = mobilenet.predict(img);
+      // smallImg is new
+      const smallImg = tf.image.resizeBilinear(img, [224, 224]);
+      const activation = mobilenet.predict(smallImg);
       const predictions = model.predict(activation);
+      // new
+      img.dispose()
+      smallImg.dispose()
       return predictions.as1D().argMax();
     });
     const classId = (await predictedClass.data())[0];
     var predictionText = "";
     switch(classId){
-		case 0:
-			predictionText = "I see Rock";
-			break;
-		case 1:
-			predictionText = "I see Paper";
-			break;
-		case 2:
-			predictionText = "I see Scissors";
-			break;
-		case 3:
-			predictionText = "I see Spock";
-			break;
-            
-        // Add a case for lizard samples.
-        // HINT: Look at the previous cases.
-            
-        // YOUR CODE HERE 
-	
-            
-	}
-	document.getElementById("prediction").innerText = predictionText;
-			
-    
+        case 0:
+            predictionText = "I see Rock";
+            break;
+        case 1:
+            predictionText = "I see Paper";
+            break;
+        case 2:
+            predictionText = "I see Scissors";
+            break;
+        case 3:
+            predictionText = "I see Spock";
+            break;
+    case 4:
+      predictionText = "I see Lizard";
+      break;
+    }
+    document.getElementById("prediction").innerText = predictionText;
+
+
     predictedClass.dispose();
     await tf.nextFrame();
   }
 }
+
 
 
 function doTraining(){
